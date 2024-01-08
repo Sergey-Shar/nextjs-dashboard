c62a8ef0f5827a3bd8e85cb1631c0c74c3112294
Sergey Sharuha <sergeihar82@gmail.com>
2024-01-08 18:54:19 +0300

    feat: finish create app

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,8 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: 'Customers',
+}
 export default function Customers() {
     return <div className="text-blue-600">Customers</div>;
 }
--- app/dashboard/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Dashboard() {
-    return <div className="text-blue-600">Dashboard</div>;
-}
